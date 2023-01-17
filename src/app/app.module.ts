74cb3090822dd16f86fee30a35cb43fae8b80c3f
Amzad Khan <amzad.khan@esq.com>
2023-01-17 08:37:08 +0530

    chnage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 import { ProfileSummaryComponent } from './profile-summary/profile-summary.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -23,6 +28,11 @@ import {MatGridListModule} from '@angular/material/grid-list';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
